e5600a658f1cebcdf634526af03c2eed3ef50838
binly.z <binlyzhuo@outlook.com>
2017-12-24 17:48:34 +0800

    ~

---
+++ csharp_indepth/Chpt01/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chpt01
+{
+    public class Product
+    {
+
+    }
+}
