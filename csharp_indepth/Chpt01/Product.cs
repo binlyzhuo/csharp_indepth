c4eb2733c5b8f4f419dd2f85d7f8b951d31920a9
binly.z <binlyzhuo@outlook.com>
2017-12-24 18:04:31 +0800

    ~

--- csharp_indepth/Chpt01/Product.cs
+++ csharp_indepth/Chpt01/Product.cs
@@ -8,6 +8,27 @@ namespace Chpt01
 {
     public class Product
     {
+        private string name;
+        public string Name { set; get; }
 
+        private decimal price;
+
+        public decimal Price { set; get; }
+
+        public Product(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public static List<Product> GetSampleProducts()
+        {
+            List<Product> list = new List<Product>();
+            list.Add(new Product("West Side Story",9.9m));
+            list.Add(new Product("Assassins",14.9m));
+            list.Add(new Product("Frogs",13.9m));
+            list.Add(new Product("Sweeney Todd",10.99m));
+            return list;
+        }
     }
 }
